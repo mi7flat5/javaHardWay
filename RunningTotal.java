bfd2e3eb85ae4fe00ee5c59f4b8dc3b4e384233a
Shane Harmon <mi7flat5@gmail.com>
2017-04-10 21:26:28 +0200

    changed for drill

--- RunningTotal.java
+++ RunningTotal.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class RunningTotal{
 	public static void main(String[] args){
 		Scanner keyboard = new Scanner(System.in);
-		int current, total = 0;
+		int current =1, total = 0;
 		
 		System.out.print("Type in a bunch of values and I'll add them up. ");
 		System.out.println("I'll stop when you type zero. ");
@@ -12,6 +12,7 @@ public class RunningTotal{
 			System.out.print("Value: ");
 			current = keyboard.nextInt();
 			total += current;
+			if (current !=0)
 			System.out.println("The total so far is: "+ total);
 			
 		}while (current != 0);
